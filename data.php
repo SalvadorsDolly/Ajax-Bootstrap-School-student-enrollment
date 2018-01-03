db8e7abf381aa27a8fb88700d9386501a29a15ca
Marisa Cole <salvadorsdolly@gmail.com>
2018-01-02 21:04:06 -0800

    Created db.php and linked to data.php, moved html table to data.php: 3rd commit

--- data.php
+++ data.php
@@ -1,3 +1,48 @@
 <?php 
-	echo 'This is SchoolData PHP Page';
+	include 'db.php';
 ?>
+<tr>
+	<td>1</td>
+	<td>Lars</td>
+	<td>Dataprogramering</td>
+	<td>500/=</td>
+	<td>
+		<div class="dropdown">
+			<button class="btn btn-primary" data-toggle="dropdown">Handling<span class="caret"></span></button>
+			<ul class="dropdown-menu">
+				<li><a href="#">Redigera-Edit</li>
+				<li><a href="#">Radera-Delete</li>
+			</ul>
+		</div>
+	</td>
+</tr>
+<tr>
+	<td>2</td>
+	<td>Svea</td>
+	<td>Robotuppror</td>
+	<td>1000/=</td>
+	<td>
+		<div class="dropdown">
+			<button class="btn btn-primary" data-toggle="dropdown">Handling<span class="caret"></span></button>
+			<ul class="dropdown-menu">
+				<li><a href="#">Redigera-Edit</li>
+				<li><a href="#">Radera-Delete</li>
+			</ul>
+		</div>
+	</td>
+</tr>
+<tr>
+	<td>3</td>
+	<td>Sigrid</td>
+	<td>Häxa</td>
+	<td>750/=</td>
+	<td>
+		<div class="dropdown">
+			<button class="btn btn-primary" data-toggle="dropdown">Handling<span class="caret"></span></button>
+		 	<ul class="dropdown-menu">
+				<li><a href="#">Redigera-Edit</li>
+				<li><a href="#">Radera-Delete</li>
+			</ul>
+		</div>
+	</td>
+</tr>
---
+++ db.php
@@ -0,0 +1,8 @@
+<?php 
+	$server = 'localhost';
+	$user = 'Marisa';
+	$password = 'Code_girl!';
+	$db = 'school_record_project';
+
+	$conn = mysqli_connect($server, $user, $password, $db);
+?>
